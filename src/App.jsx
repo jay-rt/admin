9368c1fe88ecac53fef3a4a2fa4d94edeaa9b0ab
jay-rt <thapa.jayrt@gmail.com>
2023-02-23 19:42:53 -0500

    alternative admin panel with navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/navbar/Navbar";
+import "./variables.scss";
+import "./app.scss";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default App;
